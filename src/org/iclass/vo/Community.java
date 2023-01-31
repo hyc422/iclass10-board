0bd39aeb541c41bfa05315ab31b69b09e7f90554
Yongchee Hong <ghddydcl422@gmail.com>
2023-01-31 10:44:02 +0900

    #4 mapper.xml - vo

---
+++ src/org/iclass/vo/Community.java
@@ -0,0 +1,26 @@
+package org.iclass.vo;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Community 
+{
+	private long idx;
+	private String Writer;
+	private String title;
+	private String content;
+	private int readCount;
+	private Timestamp createdAt;
+	private String ip;
+	private String commentCount;
+}	// Class end
